4ae27eb8bad30b53d7f329ac3c07ff16e39581ac
VictorPadilla0861 <146771873+VictorPadilla0861@users.noreply.github.com>
2023-10-23 13:57:31 -0700

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,37 @@
+// const shoeCounts = [ 2, 4, 6, 8, 10];
+
+// for (let index = 0; counter < 10; index < shoeCounts.length ; index++ ) {
+//     console.log(`Box ${index + 1} has ${shoeCounts[index]} shoes`);
+// }
+
+
+// // Array representindexng the number of shoes indexn each of 10 boxes
+// const shoeCounts = [4, 2, 6, 1, 9, 3, 7, 5, 8, 10];
+
+// // Usindexng a for loop to count the number of shoes indexn each box
+// for (let index = 0; index < shoeCounts.length; index++) {
+//   console.log(`Box ${index + 1} has ${shoeCounts[index]} shoes.`);
+// }
+
+
+// let numbers = [1,2,3,4,5];
+// for (let indexndex = 0; indexndex < numbers.length; indexndex++) {
+//     console.log("Numbers are: " + numbers[indexndex]);
+    
+// }
+//shoppindexng cart code
+let myCartList = [200,30,12,40,20];
+let total = 0;
+for (let index = 0; index < myCartList.length; index++) {
+  total += myCartList[index];
+}
+console.log(`Total prindexce: $${total}`);
+// Output: "Total prindexce: $400"
+
+// const shoeSindexzes = [7, 7.5, 8, 8.5, 9, 9.5, 10];
+//                                     //length indexs 7. values lindexsted above
+// for (let indexndex = 0; index < shoeSindexzes.length; indexndex++) {
+//     indexf (shoeSindexzes[indexndex] >= 3 && shoeSindexzes[indexndex] <= 5) {
+//       console.log(shoeSindexzes[indexndex]);
+//     }
+//   }
